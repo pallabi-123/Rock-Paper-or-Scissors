c86d0e08e88d7eb6be7a0b9c85f7bcc63d975cbd
Pallabi Dutta Bose <83059771+pallabi-123@users.noreply.github.com>
2022-07-19 19:51:02 +0530

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,9 +1,19 @@
 /*1.	The user should be able to choose ‘rock’, ‘paper’ or ‘scissors’ when the game starts.
 Using const and arrow function syntax, create a function named getUserChoice that takes a single parameter userInput.*/
-
 Const  getUserChoice = userInput => {
+ 
 /* 2) Since user can pass a parameter, such as ‘Rock’ or ‘rock’ with different capitalization, begin by utilizing Javascript’s tolowerCase() function to make the JavaScript’s tolowerCase() function to make the userInput all lowercase.
 You can use code like this:
 userInput = userInput. tolowerCase();*/
  userInput. tolowerCase();
+ 
+ /* 3) 3. When getting the user’s choice, you should also check to make sure that he user typed a valid choice: ‘rock’, ‘paper’, or ‘scissors’.
+Inside getUserChoice(), write an if/else statement that makes sure the userInput is either ‘rock’, ‘paper’, or ‘scissors’. 
+If it does, then return the userInput. If not, use console.log to print an error message to the console.
+*/
+ If(userInput === ‘rock’ ||  userInput === ‘paper’ || userInput === ‘scissors’) {
+   return userInput;
+} else {
+   Console.log(‘You should pick between the rock, paper or scissors’);
 }
+};
